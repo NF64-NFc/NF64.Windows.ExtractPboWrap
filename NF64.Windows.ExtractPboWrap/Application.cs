fecf902b8949d99fde9311bc9850838f9724efa0
NF64-NFc <n.fushimi@nfc64.net>
2021-04-29 19:02:21 +0900

    *

---
+++ NF64.Windows.ExtractPboWrap/Application.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+
+
+namespace NF64.Windows.ExtractPboWrap
+{
+    internal static class Application
+    {
+        private static Assembly CurrentAssembly { get; } = Assembly.GetEntryAssembly();
+
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Process.Start(new ExtractPboParameter(args).GetProcessStartInfo());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, CurrentAssembly.Location, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ NF64.Windows.ExtractPboWrap/ExtractPboParameter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace NF64.Windows.ExtractPboWrap
+{
+    internal sealed class ExtractPboParameter
+    {
+        public string ExePath { get; }
+
+        public string PboFilePath { get;}
+
+
+        public ExtractPboParameter(string[] args)
+        {
+            if (args == null)
+                throw new ArgumentNullException(nameof(args));
+            if (args.Length != 2)
+                throw new ArgumentException("Need 2 Parameters. Ex: [ExePath] [PboFilePath]");
+
+            this.ExePath = LoadFilePath(args[0]?.Trim('"'));
+            this.PboFilePath = LoadPboFilePath(args[1]?.Trim('"'));
+        }
+
+
+        public ProcessStartInfo GetProcessStartInfo() => new ProcessStartInfo(this.ExePath, $"\"{this.PboFilePath}\"");
+
+
+        private static string LoadFilePath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentException($"'{path}' is null or empty.", nameof(path));
+
+            var fullPath = Path.GetFullPath(path);
+            if (Directory.Exists(fullPath))
+                throw new FileNotFoundException($"'{fullPath}' was not file.", path);
+            if (!File.Exists(fullPath))
+                throw new FileNotFoundException($"'{fullPath}' could be not found.", path);
+            return fullPath;
+        }
+
+
+        private static string LoadPboFilePath(string path)
+        {
+            var fullPath = LoadFilePath(path);
+            var ext = Path.GetExtension(fullPath);
+            if (ext != ".pbo")
+                throw new FormatException($"'{fullPath}' could be not '.pbo'.");
+            return fullPath;
+        }
+    }
+}
